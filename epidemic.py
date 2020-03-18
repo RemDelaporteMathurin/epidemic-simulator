7889d575e1df47e24c38b2ef6d0c53abafa6e396
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-03-18 16:34:40 +0100

    Some stuff

--- epidemic.py
+++ epidemic.py
@@ -2,27 +2,30 @@ from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import math
 
-dt = 1.2
-y_max = 0.3
-K_r_0 = 1/(51-32)
-K_r_minus = 0
-K_d_0 = 0.02
-K_d_plus = 0.02
 
+def calculate_epidemic(C, v, t_final):
+    '''
+    Arguments:
+    - C: Hospital capacity
+    - v: People's mobility (0 means perfect confinement)
+    - t_final: time of simulation
+    Returns: 5 lists
+    '''
+    dt = 1.2
+    K_r_0 = 1/(51-32)
+    K_r_minus = K_r_0/2
+    K_d_0 = 0.02
+    K_d_plus = K_d_0/2
 
-def K_c(v):
-    return 0.4*v
+    def K_c(v):
+        return 0.4*v
 
+    def K_r(y):
+        return K_r_0 - K_r_minus/(1+math.exp(-100*(y - C)))
 
-def K_r(y):
-    return K_r_0 - K_r_minus/(1+math.exp(-5*(y - y_max)))
+    def K_d(y):
+        return K_d_0 + K_d_plus/(1+math.exp(-100*(y - C)))
 
-
-def K_d(y):
-    return K_d_0 + K_d_plus/(1+math.exp(-5*(y - y_max)))
-
-
-def calculate_covid(K_c, t_final):
     x_n = 1
     y_n = 0.01
     z_n = 0
@@ -30,9 +33,9 @@ def calculate_covid(K_c, t_final):
 
     def equations(p):
         x, y, z, d = p
-        h = (x-x_n)/dt - (-K_c*x*y)
-        s = (y-y_n)/dt - (K_c*x*y - K_r(y) * y - K_d(y)*y)
-        r = (z-z_n)/dt - (K_r(y)*x*y)
+        h = (x-x_n)/dt - (-K_c(v)*x*y)
+        s = (y-y_n)/dt - (K_c(v)*x*y - K_r(y) * y - K_d(y)*y)
+        r = (z-z_n)/dt - (K_r(y)*y)
         m = (d-d_n)/dt - (K_d(y)*y)
         return (h, s, r, m)
 
