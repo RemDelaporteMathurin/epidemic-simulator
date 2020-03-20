9e88bcaeed4ca8ec63917eada97cccab15ccf06c
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-03-20 09:23:40 +0100

    ConnectionError + renamed variables + refactoring + pep8

--- data.py
+++ data.py
@@ -1,6 +1,13 @@
 import json
 import urllib
 import requests
+from datetime import datetime
+
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
 
 
 def get_data(country):
@@ -8,7 +15,7 @@ def get_data(country):
     url = "https://pomber.github.io/covid19/timeseries.json"
     try:
         data = json.loads(requests.get(url).text)
-    except:
+    except requests.ConnectionError:
         print('No internet ?')
         with open('data/timeseries.json', 'r') as f:
             data = json.load(f)
@@ -17,4 +24,7 @@ def get_data(country):
             time.append(entry["date"])
             cases.append(float(entry["confirmed"]) - float(entry["recovered"]))
             deaths.append(float(entry["deaths"]))
-    return time, cases, deaths
+    time_number_days = []
+    for t in time:
+        time_number_days.append(days_between(t, time[0]))
+    return time, time_number_days, cases, deaths
--- fit_any_country.py
+++ fit_any_country.py
@@ -4,28 +4,16 @@ from data import get_data
 import scipy.optimize
 from scipy import interpolate
 import csv
-from datetime import datetime
 import matplotlib.pyplot as plt
 
 import argparse
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("country", help="simulate the epidemic in this country. Ex: China")
+parser.add_argument(
+    "country",
+    help="simulate the epidemic in this country. Ex: China")
 args = parser.parse_args()
-
-
-def days_between(d1, d2):
-    d1 = datetime.strptime(d1, "%Y-%m-%d")
-    d2 = datetime.strptime(d2, "%Y-%m-%d")
-    return abs((d2 - d1).days)
-
-
-def read_ref_data(country):
-    time_cases_number_days = []
-    time, cases, deaths = get_data(country)
-    for t in time:
-        time_cases_number_days.append(days_between(t, time[0]))
-    return time, time_cases_number_days, \
-        cases, time, time_cases_number_days, deaths
 
 
 def cost_function(x):
@@ -33,39 +21,41 @@ def cost_function(x):
     v = a*10**(-b)
     K_r_0 = c*10**(-d)
     K_d_0 = e*10**(-f)
-    time, sick, healthy, recovered, deaths = calculate_epidemic(
-        C=0, v=v, x_n=x_n, y_n=y_n, t_final=60, K_r_0=K_r_0, K_r_minus=0,
-        K_d_0=K_d_0, K_d_plus=0)
-    interp_cases = interpolate.interp1d(time, sick, fill_value='extrapolate')
-    interp_deaths = interpolate.interp1d(time, deaths, fill_value='extrapolate')
+    time_sim, cases_sim, healthy_sim, recovered_sim, deaths_sim \
+        = calculate_epidemic(
+            C=0, v=v, x_n=x_n, y_n=y_n, t_final=60, K_r_0=K_r_0, K_r_minus=0,
+            K_d_0=K_d_0, K_d_plus=0)
+    interp_cases = interpolate.interp1d(
+        time_sim, cases_sim, fill_value='extrapolate')
+    interp_deaths = interpolate.interp1d(
+        time_sim, deaths_sim, fill_value='extrapolate')
     fitness = 0
     N = 0
-    for i in range(len(time_cases_number_days)):
-        if time_cases_number_days[i] > 25:
+
+    for i in range(len(time_number_days)):
+        if time_number_days[i] > 25:
             coeff = 1
         else:
             coeff = 1
-        fitness += coeff*abs(sick_ref[i] - interp_cases(time_cases_number_days[i]))/max(sick_ref)
-        N += coeff
-    for i in range(len(time_deaths_number_days)):
-        if time_cases_number_days[i] > 25:
-            coeff = 1
-        else:
-            coeff = 1
-        fitness += coeff*abs(deaths_ref[i] - interp_deaths(time_deaths_number_days[i]))/max(deaths_ref)
-        N += coeff
+        fitness += coeff*abs(
+            deaths_ref[i] - interp_deaths(time_number_days[i])) / \
+            max(deaths_ref)
+        fitness += coeff*abs(
+            cases_ref[i] - interp_cases(time_number_days[i])) / \
+            max(cases_ref)
+        N += 2*coeff
 
     fitness /= 2*N
     print("Fit accuracy : " + str(fitness), end='\r')
 
     return fitness
 
+
 country = args.country
 x_n = 1e5  # initial healthy population arbitrary
-time_cases, time_cases_number_days, sick_ref, time_deaths, \
-    time_deaths_number_days, deaths_ref = read_ref_data(country)
+time, time_number_days, cases_ref, deaths_ref = get_data(country)
 
-y_n = sick_ref[0]
+y_n = cases_ref[0]
 x0 = (2.78, 6.08, 25, 1.9, 1, 2)
 print('Fitting...')
 res = scipy.optimize.minimize(cost_function, x0, method="Nelder-Mead")
@@ -76,24 +66,25 @@ print(x_opt)
 v = a*10**(-b)
 K_r_0 = c*10**(-d)
 K_d_0 = e*10**(-f)
-time, sick, healthy, recovered, deaths = calculate_epidemic(
-    C=0, v=v, x_n=x_n, y_n=y_n, t_final=60, K_r_0=K_r_0, K_r_minus=0,
-    K_d_0=K_d_0, K_d_plus=0)
+time_sim, cases_sim, healthy_sim, recovered_sim, deaths_sim \
+    = calculate_epidemic(
+        C=0, v=v, x_n=x_n, y_n=y_n, t_final=60, K_r_0=K_r_0, K_r_minus=0,
+        K_d_0=K_d_0, K_d_plus=0)
 
 plt.figure()
 plt.title(country)
 plt.ylabel("Number of actives cases")
 plt.xlabel("Days")
-plt.plot(time, sick, label="Predicted cases")
-plt.scatter(time_cases_number_days, sick_ref, label="Actual cases")
+plt.plot(time_sim, cases_sim, label="Predicted cases")
+plt.scatter(time_number_days, cases_ref, label="Actual cases")
 plt.legend()
 
 plt.figure()
 plt.title(country)
 plt.xlabel("Days")
 plt.ylabel("Number of deaths")
-plt.plot(time, deaths, label="Predicted deaths")
-plt.scatter(time_deaths_number_days, deaths_ref, label="Actual number of deaths")
+plt.plot(time_sim, deaths_sim, label="Predicted deaths")
+plt.scatter(time_number_days, deaths_ref, label="Actual number of deaths")
 plt.legend()
 
 plt.show()
