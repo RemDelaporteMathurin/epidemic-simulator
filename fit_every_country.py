79f39642e367d0cada0543991f519843199aabde
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2020-03-29 12:55:47 +0200

    fix

--- fit_every_country.py
+++ fit_every_country.py
@@ -4,7 +4,8 @@ from data import fetch_data, get_data
 if __name__ == "__main__":
     data = fetch_data()
     for country in data.keys():
-        time, time_number_days, cases_ref, deaths_ref = get_data(country)
+        time, time_number_days, cases_ref, deaths_ref, recovered_ref = \
+            get_data(country)
         if len(time) > 15:
             print(country)
             time_sim, cases_sim, healthy_sim, recovered_sim, deaths_sim = \
